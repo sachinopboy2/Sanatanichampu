e0944a96112db7b0e8cb22ef9d4e9fb74b1ec6bf
sachinopboy2 <sachin25u7@gmail.com>
2025-08-01 16:01:34 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -67,7 +67,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/shizukaofficial/shizukaxchampuoffoceal",
+    "https://github.com/sachinopboy2/Sanatanichampu",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
